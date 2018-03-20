6e8d24f67dc5c830832c4712c498ecdf79ddeb39
noribang <hoonk9387@gmail.com>
2018-03-20 08:16:29 -0500

    Created an association between article and comment models.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+	# Association to comment model-Each article has many comments.
+	has_many :comments
 	# Validates article title has at least 5 characters.
 	validates :title, presence: true, length: { minimum: 5 }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+  # Association to Article model-Each comment belongs to one article. 
   belongs_to :article
 end
